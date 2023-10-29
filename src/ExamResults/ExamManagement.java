4b9e95461805ff7e1c8af301e431e7b055bb4279
Sergiy <119933936+SergiyKochenko@users.noreply.github.com>
2023-10-29 19:32:41 +0000

    Fixing bugs

--- src/ExamResults/ExamManagement.java
+++ src/ExamResults/ExamManagement.java
@@ -17,13 +17,13 @@ class ExamManagement {
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
-            System.out.println("1. Add Student");
+            System.out.println("\u001B[32m1. Add Student");
             System.out.println("2. Add Multiple Choice Exam Result");
             System.out.println("3. Add Essay Exam Result");
-            System.out.println("4. Print Summary Result");
+            System.out.println("\u001B[31m4. Print Summary Result");
             System.out.println("5. Print Detailed Results");
             System.out.println("6. Quit");
-            System.out.print("Enter your choice(only numbers allowed): ");
+            System.out.print("\u001B[0mEnter your choice(only numbers allowed): ");
 
             try {
                 int choice = scanner.nextInt();
@@ -36,140 +36,130 @@ class ExamManagement {
 
                 switch (choice) {
                     case 1:
-                        while (true) {
-                            System.out.print("Enter Student ID: ");
-                            String studentIdString = scanner.nextLine();
-                            if (!Student.isValidStudentId(studentIdString)) {
-                                System.out.println("Invalid student ID. Please enter only numeric characters.");
-                            } else {
-                                int studentIdInt = Integer.parseInt(studentIdString);
+                        System.out.print("Enter Student ID: ");
+                        String studentIdString = scanner.nextLine();
+                        if (!Student.isValidStudentId(studentIdString)) {
+                            System.out.println("Invalid student ID. Please enter only numeric characters.");
+                            break;
+                        }
 
-                                System.out.print("Enter Student Name: ");
-                                String studentName = scanner.nextLine();
+                        int studentIdInt = Integer.parseInt(studentIdString);
 
-                                if (!Student.isValidStudentName(studentName)) {
-                                    System.out.println("Invalid student name. Please enter only alphabetic characters.");
-                                } else {
-                                    try {
-                                        Student student = new Student(studentIdInt, studentName);
-                                        students.add(student);
-                                        System.out.println("Student added successfully.");
-                                        break;  // Break out of the loop if everything is valid
-                                    } catch (StudentException e) {
-                                        System.out.println(e.getMessage());
-                                        break;  // Break out of the loop if there's an exception
-                                    }
-                                }
-                            }
+                        System.out.print("Enter Student Name: ");
+                        String studentName = scanner.nextLine();
+
+                        if (!Student.isValidStudentName(studentName)) {
+                            System.out.println("Invalid student name. Please enter only alphabetic characters.");
+                            break;
+                        }
+
+                        try {
+                            Student student = new Student(studentIdInt, studentName);
+                            students.add(student);
+                            System.out.println("Student added successfully.");
+                        } catch (StudentException e) {
+                            System.out.println(e.getMessage());
                         }
                         break;
 
                     case 2:
-                        while (true) {
-                            try {
-                                System.out.print("Enter Student ID: ");
-                                int studentId = scanner.nextInt();
-                                scanner.nextLine();
+                        try {
+                            System.out.print("Enter Student ID: ");
+                            int studentId = scanner.nextInt();
+                            scanner.nextLine();
 
-                                Student student = findStudentById(students, studentId);
-                                if (student == null) {
-                                    System.out.println("Student not found.");
-                                    break;
-                                }
+                            System.out.print("Enter Exam ID: ");
+                            int examId = scanner.nextInt();
+                            scanner.nextLine();
 
-                                System.out.print("Enter Exam ID: ");
-                                int examId = scanner.nextInt();
-                                scanner.nextLine();
+                            System.out.print("Enter Subject: ");
+                            String subject = scanner.nextLine();
 
-                                System.out.print("Enter Subject: ");
-                                String subject = scanner.nextLine();
+                            System.out.print("Enter Duration (in minutes): ");
+                            int duration = scanner.nextInt();
+                            scanner.nextLine();
 
-                                System.out.print("Enter Duration (in minutes): ");
-                                int duration = scanner.nextInt();
-                                scanner.nextLine();
+                            System.out.print("Enter Correct Answers: ");
+                            int correctAnswers = scanner.nextInt();
+                            scanner.nextLine();
 
-                                System.out.print("Enter Total Questions: ");
-                                int noQuestions = scanner.nextInt();
-                                scanner.nextLine();
+                            System.out.print("Enter Total Questions: ");
+                            int noQuestions = scanner.nextInt();
+                            scanner.nextLine();
 
-                                System.out.print("Enter Correct Answers: ");
-                                int correctAnswers = scanner.nextInt();
-                                scanner.nextLine();
-
-                                try {
-                                    Exam exam = new MultipleChoice(examId, subject, duration, correctAnswers, noQuestions);
-                                    student.addExam(exam);
-                                    double score = ((MultipleChoice) exam).calculateScore();
-                                    ExamResult result = new ExamResult(student, exam, (int) score);
-                                    examResults.add(result);
-                                    System.out.println("Multiple Choice Exam result added successfully.");
-                                    break;  // Break out of the loop if everything is valid
-                                } catch (ExamException e) {
-                                    System.out.println(e.getMessage());
-                                    break;  // Break out of the loop if there's an exception
-                                }
-                            } catch (InputMismatchException e) {
-                                System.out.println("Invalid input. Please enter only numeric characters and try again.");
-                                scanner.nextLine();
+                            Student student = findStudentById(students, studentId);
+                            if (student == null) {
+                                System.out.println("Student not found.");
+                                break;
                             }
+
+                            try {
+                                Exam exam = new MultipleChoice(examId, subject, duration, correctAnswers, noQuestions);
+                                student.addExam(exam);
+                                double score = ((MultipleChoice) exam).calculateScore();
+                                ExamResult result = new ExamResult(student, exam, (int) score);
+                                examResults.add(result);
+                                System.out.println("Multiple Choice Exam result added successfully.");
+                            } catch (ExamException e) {
+                                System.out.println(e.getMessage());
+                            }
+                        } catch (Exception e) {
+                            System.out.println("Invalid input. Please enter only numeric characters and try again.");
+                            scanner.nextLine();
                         }
                         break;
 
                     case 3:
-                        while (true) {
-                            try {
-                                System.out.print("Enter Student ID: ");
-                                int studentId = scanner.nextInt();
-                                scanner.nextLine();
+                        try {
+                            System.out.print("Enter Student ID: ");
+                            int studentId = scanner.nextInt();
+                            scanner.nextLine();
 
-                                Student student = findStudentById(students, studentId);
-                                if (student == null) {
-                                    System.out.println("Student not found.");
-                                    break;
-                                }
+                            System.out.print("Enter Exam ID: ");
+                            int examId = scanner.nextInt();
+                            scanner.nextLine();
 
-                                System.out.print("Enter Exam ID: ");
-                                int examId = scanner.nextInt();
-                                scanner.nextLine();
+                            System.out.print("Enter Subject: ");
+                            String subject = scanner.nextLine();
 
-                                System.out.print("Enter Subject: ");
-                                String subject = scanner.nextLine();
+                            System.out.print("Enter Duration (in minutes): ");
+                            int duration = scanner.nextInt();
+                            scanner.nextLine();
 
-                                System.out.print("Enter Duration (in minutes): ");
-                                int duration = scanner.nextInt();
-                                scanner.nextLine();
+                            System.out.print("Enter Essay Answer: ");
+                            String essayAnswer = scanner.nextLine();
 
-                                System.out.print("Enter Essay Answer: ");
-                                String essayAnswer = scanner.nextLine();
+                            System.out.print("Enter Grammar Marks: ");
+                            int grammar = scanner.nextInt();
+                            scanner.nextLine();
 
-                                System.out.print("Enter Grammar Marks: ");
-                                int grammar = scanner.nextInt();
-                                scanner.nextLine();
+                            System.out.print("Enter Content Marks: ");
+                            int content = scanner.nextInt();
+                            scanner.nextLine();
 
-                                System.out.print("Enter Content Marks: ");
-                                int content = scanner.nextInt();
-                                scanner.nextLine();
+                            System.out.print("Enter Word Limit: ");
+                            int wordLimit = scanner.nextInt();
+                            scanner.nextLine();
 
-                                System.out.print("Enter Word Limit: ");
-                                int wordLimit = scanner.nextInt();
-                                scanner.nextLine();
-
-                                try {
-                                    Exam exam = new Essay(examId, subject, duration, essayAnswer, grammar, content, wordLimit);
-                                    student.addExam(exam);
-                                    double score = ((Essay) exam).calculateScore();
-                                    ExamResult result = new ExamResult(student, exam, (int) score);
-                                    examResults.add(result);
-                                    System.out.println("Essay Exam result added successfully.");
-                                    break;  // Break out of the loop if everything is valid
-                                } catch (ExamException e) {
-                                    System.out.println(e.getMessage());
-                                    break;  // Break out of the loop if there's an exception
-                                }
-                            } catch (InputMismatchException e) {
-                                System.out.println("Invalid input. Please try again.");
-                                scanner.nextLine();
+                            Student student = findStudentById(students, studentId);
+                            if (student == null) {
+                                System.out.println("Student not found.");
+                                break;
                             }
+
+                            try {
+                                Exam exam = new Essay(examId, subject, duration, essayAnswer, grammar, content, wordLimit);
+                                student.addExam(exam);
+                                double score = ((Essay) exam).calculateScore();
+                                ExamResult result = new ExamResult(student, exam, (int) score);
+                                examResults.add(result);
+                                System.out.println("Essay Exam result added successfully.");
+                            } catch (ExamException e) {
+                                System.out.println(e.getMessage());
+                            }
+                        } catch (Exception e) {
+                            System.out.println("Invalid input. Please try again.");
+                            scanner.nextLine();
                         }
                         break;
 
